380e530fb086b4bbdca4b2d28c634396447a3821
Md Rafiqul Islam <md-rafiqul.islam@s2021.tu-chemnitz.de>
2022-12-30 20:47:56 +0100

    PSPIEL-80 Implement push notification
    Push notification has been implemented. In push notification service there is a funtion `sendPushNotification` used for send push notification

---
+++ modules/push-notification/push-notification.service.js
@@ -0,0 +1,44 @@
+const admin = require('firebase-admin')
+const FCM = require('fcm-notification')
+
+const serviceAccount = require('../../config/push-notification-key.json')
+const { getAllDeviceTokens } = require('../patient/patient.service')
+const certPath = admin.credential.cert(serviceAccount)
+const fcm = new FCM(certPath)
+
+/**
+ * Send push notifcation
+ *
+ * @param title Title of the notification
+ * @param body Notification message
+ * @param deviceTokens null for all device, string for single device and array for multiple device
+ * @param data An object need have string type of values
+ */
+const sendPushNotification = async (title, body, deviceTokens = null, data = null) => {
+  try {
+    const message = data ? { notification: { title, body }, data } : { notification: { title, body } }
+
+    if (!deviceTokens) {
+      deviceTokens = await getAllDeviceTokens()
+    }
+
+    if (typeof deviceTokens === 'string') {
+      message.token = deviceTokens
+      fcm.send(message, responseFromFCM)
+    } else if (!deviceTokens || Array.isArray(deviceTokens)) {
+      fcm.sendToMultipleToken(message, deviceTokens, responseFromFCM)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const responseFromFCM = (error, response) => {
+  if (error) {
+    console.log('error found', error)
+  } else {
+    console.log('Notification Sent')
+  }
+}
+
+module.exports = { sendPushNotification }

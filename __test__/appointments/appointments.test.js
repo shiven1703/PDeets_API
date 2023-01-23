f406cf4796e7f24ca0e0c1b25eaca94565746d64
Anjali Gupta <anjali.gupta@s2021.tu-chemnitz.de>
2023-01-23 21:50:47 +0100

    [PSPIEL-185] added appointment qr code tests

--- __test__/appointments/appointments.test.js
+++ __test__/appointments/appointments.test.js
@@ -1,7 +1,7 @@
 const { db } = require('../setup').database
 const appointmentService = require('../../modules/appointment/appointment.service')
 
-describe('Appointment Listing endpoint tests', () => {
+describe('Appointment endpoint tests', () => {
   let locationsList = null
   let departmentList = null
   let doctortList = null
@@ -67,6 +67,42 @@ describe('Appointment Listing endpoint tests', () => {
     const appointmentList = await appointmentService.showAppointments(patientId)
     expect(appointmentList).not.toBe(null)
     expect(appointmentList.length > 0).toBeTruthy()
+  })
+
+  afterAll(async () => {
+    await db.appointment.destroy({
+      where: {
+        id: bookedAppointment.id
+      }
+    })
+  })
+})
+
+describe('Appointment QR code tests', () => {
+  let bookedAppointment = null
+
+  beforeAll(async () => {
+    const locationId = '5f2d8da4-2e96-4800-8e4b-17a8e283c319'
+    const departmentId = 'e0c02666-69fd-4c05-b444-1bb8ac481539'
+    const doctorId = '4953d960-963f-41e1-a905-f41b026f8760'
+    const patientId = 'a7dc8fad-52d9-4a40-9982-7afa97a0f39b'
+    const appointmentTime = '2022-12-31 09:00:00+00'
+    const appointmentDuration = 30
+    const questionaryAnswer = "[{'sample': 'json'}]"
+
+    bookedAppointment = await appointmentService.bookAppointment({ locationId, departmentId, doctorId, patientId, appointmentTime, appointmentDuration, questionaryAnswer })
+  })
+
+  test('Is QR code token generated', async () => {
+    const qrCode = await appointmentService.generateAppointmentQR(bookedAppointment.id)
+    expect(qrCode).not.toBe(null)
+    expect(typeof qrCode.token).toBe('string')
+  })
+
+  test('Is QR code token decode working', async () => {
+    const appointment = await appointmentService.decodeAppointmentQR({ appointmentId: bookedAppointment.id, status: 'done' })
+    expect(appointment).not.toBe(null)
+    expect(appointment.status === 'done').toBeTruthy()
   })
 
   afterAll(async () => {

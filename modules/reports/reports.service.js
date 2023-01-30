954f4dec1f709aded9d9cec83102a0d1e7ac2eee
Shivam Patel <shivam.patel@s2021.tu-chemnitz.de>
2023-01-30 16:30:50 +0100

    [PSPIEL-226] added lab report delete and update endpoint

--- modules/reports/reports.service.js
+++ modules/reports/reports.service.js
@@ -3,6 +3,7 @@ const { db } = require('../../db')
 
 // lib import
 const config = require('config')
+const fs = require('fs')
 
 // custom eror
 const {
@@ -10,17 +11,26 @@ const {
 } = require('../../utils/customErrors')
 
 const addReport = async (reportData, reportFiles) => {
-  let report = await db.lab_report.create({
-    patient_id: reportData.patientId,
-    appointment_id: reportData.appointmentId,
-    report_name: reportData.reportName,
-    generation_date: reportData.generationDate,
-    report_files: reportFiles,
-    status: reportData.status,
-    remarks: reportData.remarks
-  })
-  report = addReportDownloadUrl(report)
-  return report
+  try {
+    let report = await db.lab_report.create({
+      patient_id: reportData.patientId,
+      appointment_id: reportData.appointmentId,
+      report_name: reportData.reportName,
+      generation_date: reportData.generationDate,
+      report_files: reportFiles,
+      status: reportData.status,
+      remarks: reportData.remarks
+    })
+    report = addReportDownloadUrl(report)
+    return report
+  } catch (err) {
+    reportFiles.forEach((file) => {
+      removeReportFile(file.path)
+    })
+    if (err.name === 'SequelizeForeignKeyConstraintError') {
+      throw new DatabaseError('Invalid appointment or patient id')
+    }
+  }
 }
 
 const getReports = async (patientId) => {
@@ -38,11 +48,66 @@ const getReports = async (patientId) => {
   return reports
 }
 
-const deleteReport = async (patientId, reportId) => {
+const updateReport = async (reportData, reportFiles) => {
+  let report = {}
+  if (reportFiles.length > 0) {
+    // removing old files
+    report = await db.lab_report.findOne({
+      where: {
+        id: reportData.reportId
+      },
+      raw: true
+    })
+
+    if (report) {
+      report.report_files.forEach((file) => {
+        removeReportFile(file.path)
+      })
+    } else {
+      throw new DatabaseError('No report found with the provided id or no updated were supplied.')
+    }
+
+    // updating data
+    let updatedReport = await db.lab_report.update({
+      patient_id: reportData.patientId,
+      appointment_id: reportData.appointmentId,
+      report_name: reportData.reportName,
+      generation_date: reportData.generationDate,
+      report_files: reportFiles,
+      status: reportData.status,
+      remarks: reportData.remarks
+    }, {
+      where: {
+        id: reportData.reportId
+      },
+      returning: true,
+      raw: true
+    })
+
+    updatedReport = addReportDownloadUrl(updatedReport[1][0])
+    return updatedReport
+  } else {
+    report = await db.lab_report.create({
+      patient_id: reportData.patientId,
+      appointment_id: reportData.appointmentId,
+      report_name: reportData.reportName,
+      generation_date: reportData.generationDate,
+      status: reportData.status,
+      remarks: reportData.remarks
+    })
+
+    if (report.length > 0 && report[1]) {
+      return report[1][0]
+    } else {
+      throw new DatabaseError('No report found with the provided id or no updated were supplied.')
+    }
+  }
+}
+
+const deleteReport = async (reportId) => {
   const isDeleted = await db.lab_report.destroy({
     where: {
-      id: reportId,
-      patient_id: patientId
+      id: reportId
     }
   })
 
@@ -61,8 +126,14 @@ const addReportDownloadUrl = async (report) => {
   return report
 }
 
+const removeReportFile = (filePath) => {
+  fs.unlinkSync(filePath)
+}
+
 module.exports = {
   addReport,
   getReports,
-  deleteReport
+  updateReport,
+  deleteReport,
+  removeReportFile
 }

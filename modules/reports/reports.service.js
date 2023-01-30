42019cd5f5c24c310b5abdb4f00395ab371dae59
Shivam Patel <shivam.patel@s2021.tu-chemnitz.de>
2023-01-30 15:16:48 +0100

    [PSPIEL-225] added reports listing endpoint

--- modules/reports/reports.service.js
+++ modules/reports/reports.service.js
@@ -18,6 +18,21 @@ const addReport = async (reportData, reportFiles) => {
   return report
 }
 
+const getReports = async (patientId) => {
+  let reports = await db.lab_report.findAll({
+    where: {
+      patient_id: patientId
+    },
+    raw: true
+  })
+  reports = await Promise.all(reports.map(async (report) => {
+    const updatedReport = await addReportDownloadUrl(report)
+    return updatedReport
+  }))
+
+  return reports
+}
+
 const addReportDownloadUrl = async (report) => {
   const updatedReportFiles = report.report_files.map((file) => {
     file.download_url = process.env.HOST + '/' + config.get('modules.reports.lab.upload_location').replace('public/', '') + '/' + file.filename
@@ -28,5 +43,6 @@ const addReportDownloadUrl = async (report) => {
 }
 
 module.exports = {
-  addReport
+  addReport,
+  getReports
 }

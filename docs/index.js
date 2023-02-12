d7af4336498f35a96c98eeb694b95f3b09fdc098
Anjali Gupta <anjali.gupta@s2021.tu-chemnitz.de>
2023-02-12 19:22:12 +0100

    added data requirment docs

--- docs/index.js
+++ docs/index.js
@@ -1,9 +1,19 @@
 const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
 const swaggerDocument = YAML.load('swagger.yaml')
+const apiSpecSwaggerDocument = YAML.load('docs/api_spec_swagger.yaml')
 
 module.exports = (app) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  app.use('/api-docs', swaggerUi.serveFiles(swaggerDocument), swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: {
+      requestInterceptor: (request) => {
+        request.header.origin = `http://localhost:${process.env.PORT}`
+        return request
+      }
+    }
+  }))
+
+  app.use('/api-spec', swaggerUi.serveFiles(apiSpecSwaggerDocument), swaggerUi.setup(apiSpecSwaggerDocument, {
     swaggerOptions: {
       requestInterceptor: (request) => {
         request.header.origin = `http://localhost:${process.env.PORT}`

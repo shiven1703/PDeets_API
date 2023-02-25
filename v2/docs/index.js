e1e38a352f52dfabadf21a515f5c8372d6127ec3
Ganesh Chile <ganesh.chile@s2021.tu-chemnitz.de>
2023-02-25 14:08:23 +0100

    [PSPIEL-95] updated docs uri

--- v2/docs/index.js
+++ v2/docs/index.js
@@ -1,19 +1,9 @@
 const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
 const swaggerDocument = YAML.load('swagger.yaml')
-const apiSpecSwaggerDocument = YAML.load('docs/api_spec_swagger.yaml')
 
 module.exports = (app) => {
-  app.use('/api-docs', swaggerUi.serveFiles(swaggerDocument), swaggerUi.setup(swaggerDocument, {
-    swaggerOptions: {
-      requestInterceptor: (request) => {
-        request.header.origin = `http://localhost:${process.env.PORT}`
-        return request
-      }
-    }
-  }))
-
-  app.use('/api-spec', swaggerUi.serveFiles(apiSpecSwaggerDocument), swaggerUi.setup(apiSpecSwaggerDocument, {
+  app.use('/v2/api-docs', swaggerUi.serveFiles(swaggerDocument), swaggerUi.setup(swaggerDocument, {
     swaggerOptions: {
       requestInterceptor: (request) => {
         request.header.origin = `http://localhost:${process.env.PORT}`
